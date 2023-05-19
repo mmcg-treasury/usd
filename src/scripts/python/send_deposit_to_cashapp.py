7287a61b9c33a71ac527f25f7301124dccd65d4c
THE ADMINISTRATOR <125167611+rootgov@users.noreply.github.com>
2023-05-19 15:00:55 -0700

    Update send_deposit_to_cashapp.py

--- src/scripts/python/send_deposit_to_cashapp.py
+++ src/scripts/python/send_deposit_to_cashapp.py
@@ -9,10 +9,19 @@ def send_deposit_to_cashapp(name, cashapp_username, amount, description):
         "description": description
     }
 
-    # Send API request to send the deposit to Cash App
-    response = requests.post("https://api.example.com/send_deposit_to_cashapp", json=payload)
+    try:
+        # Send API request to send the deposit to Cash App
+        response = requests.post("https://api.example.com/send_deposit_to_cashapp", json=payload)
+        response.raise_for_status()  # Raise an exception for non-2xx responses
 
-    # Handle the response and perform necessary error checking and logging
+        # Handle the response and perform necessary error checking and logging
+        if response.json().get("status") == "success":
+            print("Deposit successfully sent to Cash App.")
+        else:
+            print("Failed to send deposit to Cash App.")
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred while sending deposit to Cash App:", str(e))
 
 # Example usage:
 send_deposit_to_cashapp("Jane Smith", "$janedoe", 250.0, "Payment for goods")
