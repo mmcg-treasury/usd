3a918c54bc71481ac317569cd01bc76776c85277
THE ADMINISTRATOR <125167611+rootgov@users.noreply.github.com>
2023-05-19 15:00:31 -0700

    Update send_deposit_to_debit_card.py

--- src/scripts/python/send_deposit_to_debit_card.py
+++ src/scripts/python/send_deposit_to_debit_card.py
@@ -9,10 +9,19 @@ def send_deposit_to_debit_card(name, debit_card_number, amount, description):
         "description": description
     }
 
-    # Send API request to send the deposit to the debit card
-    response = requests.post("https://api.example.com/send_deposit_to_debit_card", json=payload)
+    try:
+        # Send API request to send the deposit to the debit card
+        response = requests.post("https://api.example.com/send_deposit_to_debit_card", json=payload)
+        response.raise_for_status()  # Raise an exception for non-2xx responses
 
-    # Handle the response and perform necessary error checking and logging
+        # Handle the response and perform necessary error checking and logging
+        if response.json().get("status") == "success":
+            print("Deposit successfully sent to debit card.")
+        else:
+            print("Failed to send deposit to debit card.")
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred while sending deposit to debit card:", str(e))
 
 # Example usage:
 send_deposit_to_debit_card("John Doe", "1234567890123456", 150.0, "Refund")
